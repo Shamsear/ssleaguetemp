22cf93fc54d206ac9a3068e142d8491ddc1c75c1
Shamsear <4pa20cs104@pace.edu.in>
2026-07-28 22:04:20 +0300

    Create client.ts

---
+++ lib/firebase/client.ts
@@ -0,0 +1,6 @@
+/**
+ * Firebase Client - Re-exports for client-side components
+ * This file provides a convenient import path for client components
+ */
+
+export { app, auth, db, storage, realtimeDb } from './config';
